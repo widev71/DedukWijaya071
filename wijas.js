093d9386a158e74e680e5ed848102176206b2fe5
I Made Wijaya <118777889+DedukWijaya071@users.noreply.github.com>
2023-06-12 22:09:52 +0800

    Add files via upload

---
+++ wijas.js
@@ -0,0 +1,25 @@
+// Mendapatkan elemen ikon menggunakan ID
+const tombol1 = document.getElementById("tomboll");
+const tombol2 = document.getElementById("tombol2");
+const tombol3 = document.getElementById("tombol3");
+const tombol4 = document.getElementById("tombol4");
+
+// Menambahkan pendengar acara klik ke ikon pertama
+tombol1.addEventListener('click', function(){
+    window.location.href = "https://t.me/Duuckme";
+});
+
+// Menambahkan pendengar acara klik ke ikon kedua
+tombol2.addEventListener('click', function(){
+    window.location.href = "https://twitter.com/dedukme?t=cOOqkZzEly1PiELna6q_tA&s=08";
+});
+
+// Menambahkan pendengar acara klik ke ikon ketiga
+tombol3.addEventListener('click', function(){
+    window.location.href = "https://vk.com/id749913673";
+});
+
+// Menambahkan pendengar acara klik ke ikon keempat
+tombol4.addEventListener('click', function(){
+    window.location.href = "https://discord.gg/aKqSzAn9";
+});
